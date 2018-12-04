b2ecf73229ba1e6887496c69014fff4cbf9554f8
Nathaniel Graham <ngraham20@my.whitworth.edu>
2018-12-04 13:05:46 -0300

    Added Hal-RTOS project. It works!

---
+++ RTOSHALDISC/Debug/HAL_Driver/Src/stm32f4xx_hal_msp_template.d
@@ -0,0 +1,2 @@
+HAL_Driver/Src/stm32f4xx_hal_msp_template.o: \
+ ../HAL_Driver/Src/stm32f4xx_hal_msp_template.c
---
+++ RTOSHALDISC/Debug/Middlewares/Third_Party/FreeRTOS/Source/list.d
@@ -0,0 +1,26 @@
+Middlewares/Third_Party/FreeRTOS/Source/list.o: \
+ ../Middlewares/Third_Party/FreeRTOS/Source/list.c \
+ C:/Users/loder/Documents/repos/TicTacToeEmbedded/TTT/TTTEmbeddedSystems/RTOSHALDISC/Middlewares/Third_Party/FreeRTOS/Source/include/FreeRTOS.h \
+ C:/Users/loder/Documents/repos/TicTacToeEmbedded/TTT/TTTEmbeddedSystems/RTOSHALDISC/inc/FreeRTOSConfig.h \
+ C:/Users/loder/Documents/repos/TicTacToeEmbedded/TTT/TTTEmbeddedSystems/RTOSHALDISC/Middlewares/Third_Party/FreeRTOS/Source/include/projdefs.h \
+ C:/Users/loder/Documents/repos/TicTacToeEmbedded/TTT/TTTEmbeddedSystems/RTOSHALDISC/Middlewares/Third_Party/FreeRTOS/Source/include/portable.h \
+ C:/Users/loder/Documents/repos/TicTacToeEmbedded/TTT/TTTEmbeddedSystems/RTOSHALDISC/Middlewares/Third_Party/FreeRTOS/Source/include/deprecated_definitions.h \
+ C:/Users/loder/Documents/repos/TicTacToeEmbedded/TTT/TTTEmbeddedSystems/RTOSHALDISC/Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM4F/portmacro.h \
+ C:/Users/loder/Documents/repos/TicTacToeEmbedded/TTT/TTTEmbeddedSystems/RTOSHALDISC/Middlewares/Third_Party/FreeRTOS/Source/include/mpu_wrappers.h \
+ C:/Users/loder/Documents/repos/TicTacToeEmbedded/TTT/TTTEmbeddedSystems/RTOSHALDISC/Middlewares/Third_Party/FreeRTOS/Source/include/list.h
+
+C:/Users/loder/Documents/repos/TicTacToeEmbedded/TTT/TTTEmbeddedSystems/RTOSHALDISC/Middlewares/Third_Party/FreeRTOS/Source/include/FreeRTOS.h:
+
+C:/Users/loder/Documents/repos/TicTacToeEmbedded/TTT/TTTEmbeddedSystems/RTOSHALDISC/inc/FreeRTOSConfig.h:
+
+C:/Users/loder/Documents/repos/TicTacToeEmbedded/TTT/TTTEmbeddedSystems/RTOSHALDISC/Middlewares/Third_Party/FreeRTOS/Source/include/projdefs.h:
+
+C:/Users/loder/Documents/repos/TicTacToeEmbedded/TTT/TTTEmbeddedSystems/RTOSHALDISC/Middlewares/Third_Party/FreeRTOS/Source/include/portable.h:
+
+C:/Users/loder/Documents/repos/TicTacToeEmbedded/TTT/TTTEmbeddedSystems/RTOSHALDISC/Middlewares/Third_Party/FreeRTOS/Source/include/deprecated_definitions.h:
+
+C:/Users/loder/Documents/repos/TicTacToeEmbedded/TTT/TTTEmbeddedSystems/RTOSHALDISC/Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM4F/portmacro.h:
+
+C:/Users/loder/Documents/repos/TicTacToeEmbedded/TTT/TTTEmbeddedSystems/RTOSHALDISC/Middlewares/Third_Party/FreeRTOS/Source/include/mpu_wrappers.h:
+
+C:/Users/loder/Documents/repos/TicTacToeEmbedded/TTT/TTTEmbeddedSystems/RTOSHALDISC/Middlewares/Third_Party/FreeRTOS/Source/include/list.h:
